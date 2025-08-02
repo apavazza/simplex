44f742c416d069049032d644485cbbf1c19bd82d
apavazza <115418293+apavazza@users.noreply.github.com>
2025-08-02 13:16:14 +0200

    Fixed string quotes in metadata title for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import Footer from '@/src/components/footer'
 
 export const metadata: Metadata = {
-  title: 'Simplex Method Solver',
+  title: "Simplex Method Solver",
   description: "Open-source web app to solve linear programming problems with the Simplex algorithm. Interactive interface with step-by-step solutions, visualizations, and support for standard and dual simplex methods.",
 }
 
