1f71c37610339eeca7f54e8bd3a6d22e625ffd05
apavazza <115418293+apavazza@users.noreply.github.com>
2025-10-23 00:34:20 +0200

    Added robots meta tag to prevent indexing and updated dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import Footer from '@/src/components/footer'
 export const metadata: Metadata = {
   title: "Simplex Method Solver",
   description: "Open-source web app to solve linear programming problems with the Simplex algorithm. Interactive interface with step-by-step solutions, visualizations, and support for standard and dual simplex methods.",
+  robots: {
+    index: false,
+    follow: false,
+  },
 }
 
 export default function RootLayout({
